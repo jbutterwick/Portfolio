2e10601d92b15ea3affea6bca1e6688b5465d9de
Jordan Butterwick <33608116+Jordanwbutterwick@users.noreply.github.com>
2019-02-21 15:31:36 -0700

    init php files

---
+++ php/links.php
@@ -0,0 +1,17 @@
+<head>
+    <link
+      href="https://fonts.googleapis.com/css?family=Cormorant:300,300i|Montserrat:300,500"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
+      integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
+      crossorigin="anonymous"
+    />
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="./styles/styles.css" />
+    <title>Jordan Butterwick</title>
+  </head>
---
+++ php/main.php
@@ -0,0 +1,15 @@
+
+    <main>
+      <div class="hero">
+        <div class="hero-background">
+          <div class="content">
+            <h1>Jordan Butterwick</h1>
+            <h2>email: jordanwbutterwick@gmail.com</h3>
+            <h2>
+                <a href="https://www.linkedin.com/in/jordanwb/" class="link">linkedin</a>
+            </h3>
+          </div>
+        </div>
+      </div>
+      <div class="future-project-box"></div>
+    </main>
