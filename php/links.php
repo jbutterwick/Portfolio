456d73a9436c0defb4a9480734991dab0052647f
Jordan Butterwick <33608116+Jordanwbutterwick@users.noreply.github.com>
2019-02-28 16:31:21 -0700

    Update links.php

--- php/links.php
+++ php/links.php
@@ -12,6 +12,6 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link rel="stylesheet" href="./styles/styles.css" />
+    <link rel="stylesheet" href="../styles/styles.css" />
     <title>Jordan Butterwick</title>
   </head>
