0e738359f342a7a6a90d002abc3f06bbd86d8fba
Jordan Butterwick <33608116+Jordanwbutterwick@users.noreply.github.com>
2019-03-03 14:31:26 -0700

    updating styles

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,31 +7,31 @@
 // }
 
 // loop();
-
-var submit = document
-  .getElementById("submit")
-  .addEventListener("click", function(event) {
-    event.preventDefault();
-    var artistName = document.getElementById("artist").value;
-    var songTitle = document.getElementById("song").value;
-    var url = "https://api.lyrics.ovh/v1";
-    var newUrl = url + "/" + artistName + "/" + songTitle;
-    var results = document.getElementById("results");
-    var hero2 = document.getElementById("hero-2");
-    fetch(newUrl)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        //   made it to the actual data
-        var lyrics = data.lyrics.replace(/\n/g, "<br />");
-        results.innerHTML = "<h2>Results:</h2>" + "<p>" + lyrics + "</p>";
-        hero2.innerHTML;
-        window.scrollBy(0, 1000);
-        console.log(data);
-      });
-  });
-
+if (document.getElementById("submit")) {
+  var submit = document
+    .getElementById("submit")
+    .addEventListener("click", function(event) {
+      event.preventDefault();
+      var artistName = document.getElementById("artist").value;
+      var songTitle = document.getElementById("song").value;
+      var url = "https://api.lyrics.ovh/v1";
+      var newUrl = url + "/" + artistName + "/" + songTitle;
+      var results = document.getElementById("results");
+      var hero2 = document.getElementById("hero-2");
+      fetch(newUrl)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(data) {
+          //   made it to the actual data
+          var lyrics = data.lyrics.replace(/\n/g, "<br />");
+          results.innerHTML = "<h2>Results:</h2>" + "<p>" + lyrics + "</p>";
+          hero2.innerHTML;
+          window.scrollBy(0, 1000);
+          console.log(data);
+        });
+    });
+}
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function myFunction() {
