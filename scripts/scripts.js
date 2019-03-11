3f0b8cb353f2abbb37c18339efbf889dce1546e1
Jordan Butterwick <33608116+Jordanwbutterwick@users.noreply.github.com>
2019-03-11 15:33:00 -0600

    lyric finder style changes to conform with homepage

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,29 +8,27 @@
 
 // loop();
 if (document.getElementById("submit")) {
-  var submit = document
-    .getElementById("submit")
-    .addEventListener("click", function(event) {
-      event.preventDefault();
-      var artistName = document.getElementById("artist").value;
-      var songTitle = document.getElementById("song").value;
-      var url = "https://api.lyrics.ovh/v1";
-      var newUrl = url + "/" + artistName + "/" + songTitle;
-      var results = document.getElementById("results");
-      var hero2 = document.getElementById("hero-2");
-      fetch(newUrl)
-        .then(function(response) {
-          return response.json();
-        })
-        .then(function(data) {
-          //   made it to the actual data
-          var lyrics = data.lyrics.replace(/\n/g, "<br />");
-          results.innerHTML = "<h2>Results:</h2>" + "<p>" + lyrics + "</p>";
-          hero2.innerHTML;
-          window.scrollBy(0, 1000);
-          console.log(data);
-        });
-    });
+  var submit = document.getElementById("submit").addEventListener("click", function(event) {
+    event.preventDefault();
+    var artistName = document.getElementById("artist").value;
+    var songTitle = document.getElementById("song").value;
+    var url = "https://api.lyrics.ovh/v1";
+    var newUrl = url + "/" + artistName + "/" + songTitle;
+    var results = document.getElementById("results");
+    var hero2 = document.getElementById("hero-2");
+    fetch(newUrl)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        //   made it to the actual data
+        var lyrics = data.lyrics.replace(/\n/g, "<br />");
+        results.innerHTML = "<h2>Results:</h2>" + "<p>" + lyrics + "</p>";
+        hero2.innerHTML;
+        window.scrollBy(0, 700);
+        console.log(data);
+      });
+  });
 }
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
